f9bd3f18d7784494bc08fa0213672a775b29248c
syedvaiz <vaizmehmoodi@gmail.com>
2019-12-01 12:18:22 +0530

    edited init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,6 +1,20 @@
 import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 
-const firebaseApp = firebase.initializeApp(config);
+var firebaseConfig = {
+    apiKey: "AIzaSyAelw3_W8sGFEcQzrTfMTgECrKUniEZDhs",
+    authDomain: "senor-chat.firebaseapp.com",
+    databaseURL: "https://senor-chat.firebaseio.com",
+    projectId: "senor-chat",
+    storageBucket: "senor-chat.appspot.com",
+    messagingSenderId: "239605714442",
+    appId: "1:239605714442:web:c886fdab8d68ad647459f3",
+    measurementId: "G-H7SH4Y6MSD"
+  };
+  // Initialize Firebase
+  const firebaseAPP = firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  firebaseApp.firestore().settings({ timestampsInSnapshots: true})
+
 
 export default firebaseApp.firestore()
